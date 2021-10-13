3d8926799cdc418a2bd22df96a04bf764d24ca57
julianprada01 <julianprada01@gmail.com>
2021-10-12 23:56:44 -0500

    server

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '757241427d821c258905a65db91bb280'),
+    },
+  },
+});
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '00b15176-6195-4128-ae2e-9acbd55d6ee7'
+};
